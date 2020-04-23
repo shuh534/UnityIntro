669664880399eec0b82fb37a615c637ffbc8ef82
shuh534 <54824444+shuh534@users.noreply.github.com>
2020-04-23 02:05:54 -0400

    Simple camera control

---
+++ CameraControl.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public float mouseSensivity = 100f;
+    public GameObject player;
+
+	void Update () {
+		float lookLeftAndRight = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensivity;
+		float lookUpAndDown = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensivity;
+		player.transform.Rotate(lookLeftAndRight, lookUpAndDown, 0);
+	}
+}
